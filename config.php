5ca61dd17b59216389916954073aa84b5281f60b
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-22 09:29:51 +0700

    $conn->query(

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
-$connection=mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'sederhana', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
-if (!$connection){
+mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'sederhana', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+if (!$conn){
         die("Connection Failed:".mysqli_connect_error());
     }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include "config.php";
-$query = mysqli_query($connection,"SELECT * FROM rsh_admin ORDER BY id DESC");
+$conn->query("SELECT * FROM rsh_admin");
 ?>
 <form action="" method="post">
     <table border="1" cellpadding="0" cellspacing="0">
