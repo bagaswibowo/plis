534bbf67132eb488c111cfadeedd73a7985cc58f
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-22 09:16:38 +0700

    connection

--- config.php
+++ config.php
@@ -1,10 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "latihan";
-
-$connection = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+$connection=mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'sederhana', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 if (!$connection){
         die("Connection Failed:".mysqli_connect_error());
     }
